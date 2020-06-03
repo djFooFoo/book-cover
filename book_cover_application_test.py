2d5a064c0e8c768614795d1b7225ff2a21aca9d8
Dieter <dieter.jordens@continuum.be>
2020-06-03 12:54:18 +0200

    update test to last version and prevent new image from being downloaded.

--- book_cover_application_test.py
+++ book_cover_application_test.py
@@ -5,15 +5,15 @@ import requests
 
 class BookCoverApplicationTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.uuid = '0743269519'
+        self.uuid = '9780062803832'
         self.url = f"http://0.0.0.0:10000/book-cover/{self.uuid}"
         self.response: requests.Response = requests.get(url=self.url)
 
     def test_returns_ok(self) -> None:
         self.assertEqual(200, self.response.status_code)
 
-    def test_returns_header_application_pdf(self) -> None:
-        self.assertEqual('image/jpeg', self.response.headers['content-type'])
+    def test_returns_header_text_html_utf8(self) -> None:
+        self.assertEqual('text/html; charset=utf-8', self.response.headers['content-type'])
 
     def test_returns_string(self) -> None:
         self.assertIsInstance(self.response.content, bytes)
