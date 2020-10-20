d44e008fcaa682050cfa57b366c51622c4cd31af
dieter.jordens <dieter.jordens@continuum.be>
2020-10-20 10:58:39 +0200

    updates

--- src/book_cover.py
+++ src/book_cover.py
@@ -10,22 +10,23 @@ class BookCover:
     def get_by(self, isbn: str) -> bytes:
         internal_url = f'http://www.dieterjordens.be:10003/images/{isbn}'
         book_result_internal_library = self.request_library.get(internal_url)
-        print(book_result_internal_library)
+        print(book_result_internal_library.status_code)
         if book_result_internal_library.status_code == 200:
             return base64.decodebytes(book_result_internal_library.content)
 
         external_url = f'http://covers.openlibrary.org/b/isbn/{isbn}-M.jpg?default=false'
         book_result_external_library = self.request_library.get(external_url)
-        print(book_result_external_library)
+        print(book_result_external_library.status_code)
         if book_result_external_library.status_code == 200:
             image = book_result_external_library.content
-
-            image_data = {
-                'id': isbn,
-                'base64String': base64.b64encode(image).decode('utf-8')
-            }
-            print(self.request_library.post(url='http://www.dieterjordens.be:10003/images/', json=image_data))
-
+            self.add_image(image, isbn)
             return image
         else:
             raise RuntimeError(f'No image found for the following isbn number: `{isbn}`')
+
+    def add_image(self, image, isbn):
+        image_data = {
+            'id': isbn,
+            'base64String': base64.b64encode(image).decode('utf-8')
+        }
+        print(self.request_library.post(url='http://www.dieterjordens.be:10003/images/', json=image_data))
